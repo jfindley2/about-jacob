66d81e92c183f8c1c07e2fd8400f74356335cf93
Jacob Findley <jfindley2@cnm.edu>
2016-01-13 14:27:33 -0700

    Added styles

--- index.php
+++ index.php
@@ -3,34 +3,33 @@
 	<head>
 		<meta charset="UTF-8"/>
 		<title>Regarding Jacob Findley</title>
+		<link rel="stylesheet" href="css/styles.css"/>
 	</head>
 	<body>
+		<!-- This is my web page -->
 		<header>
 			Jacob Findley: Reasonably Decent Programmer
 		</header>
 		<h1>Hello, visitor. This is a simplistic web page.</h1>
 		<p>I would include one of those captcha things to make sure only humans could access this page, but you know what?</p>
 		<p>No. I don't believe in discrimination. Artificial Intelligences, welcome!</p>
-		<p>(Um, I'd like to apologise on behalf of humanity if your first contact with us was through the internet.
-		If so, you probably received insults, were described with derogatory and often contradictory epithets, and
-		had your ancestry questioned. It probably wasn't personal. Most of us aren't like that when in person.)</p>
+		<p>(Um, I'd like to apologise on behalf of humanity if your first contact with us was through the internet. If so, you probably received insults, were described with derogatory and often contradictory epithets, and had your ancestry questioned. It probably wasn't personal. Most of us aren't like that when in person.)</p>
 
 		<h2>Details about me:</h2>
 		<ul>
 			<li>I am fond of writing and mathematics. I'm proud of myself for my skills in these areas.</li>
-			<li>I enjoy running and boxing. I kinda suck in these areas.</li>
+			<li>I enjoy running and boxing. Of my skills in these areas, no comment.</li>
 			<li>I have two dogs.</li>
 			<li>(Cats are nice, too.)</li>
 			<li>You can call me Jake.</li>
 			<li>I secretly wonder what it would be like if my nickname was Jack instead of Jake.</li>
-			<li>One of my pet peeves is the fact that food products need to be advertised as, "Organic." That's like
-			advertising water by the fact that <em>yours</em> contains hydrogen atoms. Technically true, but unhelpful.</li>
+			<li>One of my pet peeves is the fact that food products need to be advertised as, "Organic." That's like advertising water by the fact that <em>yours</em> contains hydrogen atoms. Technically true, but unhelpful.</li>
 			<li>I have dumb pet peeves.</li>
 			<li>I am probably not as smart as I think I am.</li>
 			<li>In classes, my connate timidity is constantly in conflict with my curiosity.</li>
 			<li>In my experience, people are generally nice and have understandable reasons for what they do or believe.
-			I recognise that there are many exceptions, though.</li>
-			<li>I </li>
+			I understand that there are exceptions, though.</li>
+			<li>I can recognise myself in a mirror, and have demonstrated the ability to create and use tools.</li>
 		</ul>
 	</body>
 </html>
