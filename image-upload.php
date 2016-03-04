e7a8e6418ff60fc38f3febdd1d0d7841875cd67f
Jacob Findley <jfindley2@cnm.edu>
2016-03-04 12:09:30 -0700

    I used this file to help with image upload.

--- image-upload.php
+++ image-upload.php
@@ -4,7 +4,7 @@ $reply = new stdClass();
 $reply->status = 200;
 
 $requestContent = file_get_contents("php://input");
-$requestObject = json_decode($requestContent);
+$requestObject = json_encode($_FILES);
 $reply->data = $requestObject;
 
 $reply->message = "This worked. Or didn't and you somehow screwed it up so much you got a false positive. Either way, good job.";
